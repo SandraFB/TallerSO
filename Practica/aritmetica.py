aa03e981e9aa5b52782accd4b3a989137328f8ca
Sandra Lucia <sdelafuenteb1400@alumno.ipn.mx>
2021-09-17 17:08:35 -0500

    Create aritmetica.py

---
+++ Practica/aritmetica.py
@@ -0,0 +1,10 @@
+import numpy
+## Define las variables independientes
+a= 1.03
+b=503
+c= 2** 2
+d= b+(c*a)
+
+## Imprime los valores de las variables
+print("a= "+str(a)+" b= "+str(b)+" c= "+str(c)+" d= "+str(d))
+
