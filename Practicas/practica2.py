0848bd6dfe1617d2d156b2e2ec224ce18befdfef
Sandra Lucia <sdelafuenteb1400@alumno.ipn.mx>
2021-09-17 17:25:03 -0500

    Update and rename Practica/aritmetica.py to Practicas/practica2.py

--- Practicas/practica2.py
+++ Practicas/practica2.py
@@ -1,9 +1,12 @@
-import numpy
+import sys
+
+print("Práctica 2")
+
 ## Define las variables independientes
 a= 1.03
 b=503
 c= 2** 2
-d= b+(c*a)
+d= a*b
 
 ## Imprime los valores de las variables
 print("a= "+str(a)+" b= "+str(b)+" c= "+str(c)+" d= "+str(d))
